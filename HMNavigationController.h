d56cb3911f96c10c1033a907c6948817cd5db063
jobsyu <1175585627@qq.com>
2015-08-04 19:10:21 +0800

    touchRemoveVc

---
+++ HMNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  HMNavigationController.h
+//  TouchRemoveVc
+//
+//  Created by jobs on 15/8/4.
+//  Copyright (c) 2015年 jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMNavigationController : UINavigationController
+
+@end
